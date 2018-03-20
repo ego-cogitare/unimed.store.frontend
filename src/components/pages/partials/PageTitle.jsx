b70ac50281a914010740d0db5b2a82252eabe3e7
Alexander <ego.cogitare@gmail.com>
2018-03-20 18:37:08 +0300

    ...

--- src/components/pages/partials/PageTitle.jsx
+++ src/components/pages/partials/PageTitle.jsx
@@ -4,6 +4,8 @@ import classNames from 'classnames';
 export default class PageTitle extends React.Component {
   constructor(props) {
     super(props);
+
+    this.trimTo = 80;
   }
 
   render() {
@@ -14,14 +16,14 @@ export default class PageTitle extends React.Component {
             {
               this.props.breadcumbs.map((item, key) => (
                 <span key={key} class="breadcumb">
-                  <span class={classNames({ 'color-green': key === this.props.breadcumbs.length - 1 })}>{item}</span>
+                  <span class={classNames({ 'color-green': key === this.props.breadcumbs.length - 1 })}>{(item || '').substr(0, this.trimTo)}{(item || '').length > this.trimTo ? <span>&hellip;</span> : ''}</span>
                   { key < this.props.breadcumbs.length - 1 && <span> > </span> }
                 </span>
               ))
             }
           </div>
           <div class="heading-1 fw-500">
-            {this.props.title}
+            {(this.props.title || '').substr(0, this.trimTo)}{(this.props.title || '').length > this.trimTo ? <span>&hellip;</span> : ''}
           </div>
         </div>
       </div>
